4d66706d0cf3686036e21ed645eea05e81f3956a
ViCORe <138658952+ViCORe@users.noreply.github.com>
2023-07-05 11:00:26 +0300

    Create LinuxTools.php

---
+++ class/LinuxTools.php
@@ -0,0 +1,12 @@
+<?php class LinuxTools
+{
+  public static function ping
+  (
+    $host	= '127.0.0.1',  //  IP/Host.Domain
+    $w		= 1				//	Wait 1 sec. / def.by.linux=10sec.
+  )
+  {
+    exec ( "ping -c 1 {$host}" , $output , $errorlevel );
+    return $errorlevel ? false : true;
+  }
+}?>
